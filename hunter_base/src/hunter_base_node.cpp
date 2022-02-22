b70a015c7c545287fadcf8442a02ac1cab70aa40
885288421 <tang___xi@163.com>
2022-02-22 16:03:04 +0800

    first commit

---
+++ hunter_base/src/hunter_base_node.cpp
@@ -0,0 +1,40 @@
+/*
+ * hunter_base_node.cpp
+ *
+ * Created on: Oct 15, 2021 16:20
+ * Description:
+ *
+ * Copyright (c) 2021 Weston Robot Pte. Ltd.
+ */
+
+#include <memory>
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/executor.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+#include "hunter_base/hunter_base_ros.hpp"
+
+using namespace westonrobot;
+
+std::shared_ptr<HunterBaseRos> robot;
+
+void DetachRobot(int signal) {
+  (void)signal;
+  robot->Stop();
+}
+
+int main(int argc, char **argv) {
+  // setup ROS node
+  rclcpp::init(argc, argv);
+  //   std::signal(SIGINT, DetachRobot);
+
+  robot = std::make_shared<HunterBaseRos>("hunter");
+  if (robot->Initialize()) {
+    std::cout << "Robot initialized, start running ..." << std::endl;
+    robot->Run();
+  }
+
+  return 0;
+}
